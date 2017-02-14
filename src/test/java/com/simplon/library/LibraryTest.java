9fed569bb28b56500f7b01e46b01ebeab2847da4
jerodkz <jeromedenkwitz@gmail.com>
2017-02-14 17:43:09 +0100

    ajout dans LibraryTest

--- src/test/java/com/simplon/library/LibraryTest.java
+++ src/test/java/com/simplon/library/LibraryTest.java
@@ -1,17 +1,21 @@
 package com.simplon.library;
 
-import junit.framework.Test;
+import org.junit.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+
+import java.util.ArrayList;
 
 /**
  * Unit test for simple App.
  */
 public class LibraryTest {
     @Test
-    public void mytest() {
+    public void ArrayListPutsBooks_1book() {
         //set
+        Library library = new Library();
         //test
+        ArrayList<Book> books = library.putBooks();
         //assert
     }
 }
